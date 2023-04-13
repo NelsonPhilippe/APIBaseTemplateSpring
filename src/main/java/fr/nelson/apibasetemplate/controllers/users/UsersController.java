bc902c50a4956f6af35e828613eeac8d23e31b89
NelsonPhilippe <philippenelson59@gmail.com>
2023-04-13 10:22:22 +0200

    feat(users) : change response type string to json

--- src/main/java/fr/nelson/apibasetemplate/controllers/users/UsersController.java
+++ src/main/java/fr/nelson/apibasetemplate/controllers/users/UsersController.java
@@ -1,5 +1,7 @@
 package fr.nelson.apibasetemplate.controllers.users;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import fr.nelson.apibasetemplate.controllers.users.forms.EmailForm;
 import fr.nelson.apibasetemplate.controllers.users.forms.PasswordForm;
 import fr.nelson.apibasetemplate.controllers.users.forms.UsersForm;
@@ -10,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping(value = "users", consumes= "application/json")
 public class UsersController {
 
     @Autowired
@@ -40,6 +42,47 @@ public class UsersController {
     public ResponseEntity<String> updatePassword(@PathVariable String id, @RequestBody() PasswordForm newPassword) {
         try{
             String message = this.usersService.updatePassword(id, newPassword.getOldPassword(), newPassword.getPassword());
+            return new ResponseEntity<>(message, null, 200);
+        } catch (HttpException e) {
+            return new ResponseEntity<>(e.getMessage(), null, e.getStatusCode());
+        }
+    }
+
+    @GetMapping("find/mail/{email}")
+    public ResponseEntity<JsonElement> findUserByEmail(@PathVariable String email) {
+        try{
+            JsonElement message = this.usersService.getUserByMail(email);
+            return new ResponseEntity<>(message, null, 200);
+        } catch (HttpException e) {
+            Gson gson = new Gson();
+            return new ResponseEntity<>(gson.toJsonTree(e.getMessage()), null, e.getStatusCode());
+        }
+    }
+
+    @GetMapping("find/id/{id}")
+    public ResponseEntity<JsonElement> findUserById(@PathVariable String id) {
+        try{
+            JsonElement message = this.usersService.getUserById(id);
+            return new ResponseEntity<>(message, null, 200);
+        } catch (HttpException e) {
+            Gson gson = new Gson();
+            return new ResponseEntity<>(gson.toJsonTree(e.getMessage()), null, e.getStatusCode());        }
+    }
+
+    @GetMapping("find/all")
+    public ResponseEntity<JsonElement> findAllUsers() {
+        try{
+            JsonElement message = this.usersService.getAllUsers();
+            return new ResponseEntity<>(message, null, 200);
+        } catch (HttpException e) {
+            Gson gson = new Gson();
+            return new ResponseEntity<>(gson.toJsonTree(e.getMessage()), null, e.getStatusCode());        }
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable String id) {
+        try{
+            String message = this.usersService.deleteUser(id);
             return new ResponseEntity<>(message, null, 200);
         } catch (HttpException e) {
             return new ResponseEntity<>(e.getMessage(), null, e.getStatusCode());
--- src/main/java/fr/nelson/apibasetemplate/services/users/UsersService.java
+++ src/main/java/fr/nelson/apibasetemplate/services/users/UsersService.java
@@ -1,5 +1,7 @@
 package fr.nelson.apibasetemplate.services.users;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import fr.nelson.apibasetemplate.controllers.users.forms.UsersForm;
 import fr.nelson.apibasetemplate.entities.UsersEntity;
 import fr.nelson.apibasetemplate.exceptions.HttpException;
@@ -7,6 +9,7 @@ import fr.nelson.apibasetemplate.repositories.users.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -62,6 +65,58 @@ public class UsersService {
                 return "Password successfully updated";
             }catch (Exception e) {
                 throw new HttpException("Saving in database failed", 400);
+            }
+        }
+        throw new HttpException("User not found", 404);
+    }
+
+    public String deleteUser(String id) throws HttpException {
+        Optional<UsersEntity> usersEntity = usersRepository.findById(id);
+
+        if(usersEntity.isPresent()){
+            try{
+                usersRepository.delete(usersEntity.get());
+                return "User successfully deleted";
+            }catch (Exception e) {
+                throw new HttpException("Saving in database failed", 400);
+            }
+        }
+        throw new HttpException("User not found", 404);
+    }
+
+    public JsonElement getAllUsers() throws HttpException {
+        try{
+            List<UsersEntity> entities = usersRepository.findAll();
+            Gson gson = new Gson();
+            return gson.toJsonTree(entities);
+        }catch (Exception e) {
+            throw new HttpException("Get in database failed", 400);
+        }
+    }
+
+    public JsonElement getUserById(String id) throws HttpException {
+        Optional<UsersEntity> usersEntity = usersRepository.findById(id);
+
+        if(usersEntity.isPresent()){
+            try{
+                Gson gson = new Gson();
+                return gson.toJsonTree(usersEntity.get());
+            }catch (Exception e) {
+                throw new HttpException("Get in database failed", 400);
+            }
+        }
+        throw new HttpException("User not found", 404);
+    }
+
+    public JsonElement getUserByMail(String mail) throws HttpException {
+        Optional<UsersEntity> usersEntity = usersRepository.findByEmail(mail);
+
+        if(usersEntity.isPresent()){
+            try{
+                Gson gson = new Gson();
+                return gson.toJsonTree(usersEntity.get());
+            }catch (Exception e) {
+                throw new HttpException("Get in database failed", 400);
             }
         }
         throw new HttpException("User not found", 404);
